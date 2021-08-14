563e1fda2000c94dd2224f787eb91fd8aec27914
LOuFKg4TkPff7WovEL5djgcpHFnjVsJSay <88899835+LOuFKg4TkPff7WovEL5djgcpHFnjVsJSay@users.noreply.github.com>
2021-08-14 14:33:56 +0200

    Add cli utility to test query inputs
    The cli utility will read from stdin/cin and resolved any queries the
    user types into it. Upon successful query resolution, it will call
    Intent::log_result().
    
    In a real life application, this would probably be the stage at which we
    would process the query and display something useful.

--- README.md
+++ README.md
@@ -16,4 +16,14 @@ This project requires CMake version >= 3.20. Older versions may work, but are no
 
 ## Running the cli utility
 
-The cli binary will be available in your build directory under `$PATH_TO_BUILD_DIR/bin/intent_extractor`. 
+The cli binary will be available in your build directory under `$PATH_TO_BUILD_DIR/bin/intent_extractor`. The process 
+will read from stdin/cin and reply to your queries as per instructions. E.g.:
+
+```
+How may I help you today?
+> What is the weather Today?
+Intent: Get Weather TimeDate:Today
+
+How may I help you today?
+> 
+```
--- bin/src/main.cpp
+++ bin/src/main.cpp
@@ -1,5 +1,78 @@
 #include <cstdlib>
+#include <iostream>
+
+#include <fact_intent.hpp>
+#include <lib.hpp>
+#include <weather_intent.hpp>
+
+static bool register_modules(ie::IntentExtractor& extractor) {
+    {
+        const auto register_result = extractor.intent_factory().register_builder(
+            extractor.string_interner(), std::make_shared<ie::WeatherIntentBuilder>());
+        if (register_result != ie::IntentFactoryRegistrationResult::Ok) {
+            std::cerr << "Failed to register weather intent module";
+            return false;
+        }
+    }
+    {
+        const auto register_result = extractor.intent_factory().register_builder(
+            extractor.string_interner(), std::make_shared<ie::FactIntentBuilder>());
+        if (register_result != ie::IntentFactoryRegistrationResult::Ok) {
+            std::cerr << "Failed to register weather intent module";
+            return false;
+        }
+    }
+    return true;
+}
 
 int main() {
+    ie::IntentExtractor extractor;
+    if (!register_modules(extractor)) {
+        return EXIT_FAILURE;
+    }
+
+    std::string text;
+    while (!std::cin.eof()) {
+        std::cout << "\nHow may I help you today?\n> ";
+        try {
+            std::getline(std::cin, text);
+        } catch (const std::exception&) {
+            std::cout << "Failed to read your question, please try again.\n";
+            continue;
+        }
+        if (std::cin.eof()) {
+            break;
+        }
+
+        if (text.size() == 0) {
+            std::cout << "Your query was empty, please try again.\n";
+        }
+
+        try {
+            const auto extractor_result = extractor.extract_intent(text);
+            switch (extractor_result.result) {
+                case ie::IntentExtractorResult::Ok: {
+                    std::cout << "Intent: ";
+                    extractor_result.intent->log_result(std::cout);
+                    std::cout << '\n';
+                    break;
+                }
+                case ie::IntentExtractorResult::IntentFailedToInit: {
+                    std::cout << "Failed to process query, please try again.\n";
+                    break;
+                }
+                case ie::IntentExtractorResult::CouldNotFindIntent: {
+                    std::cout << "Could not locate any intents, please try again.\n";
+                    break;
+                }
+                default:
+                    break;
+            }
+        } catch (const std::exception&) {
+            std::cout << "Something unexpected happened and we could not process your query. "
+                         "Please try again.\n";
+        }
+    }
+    std::cout << "Goodbye" << std::endl;
     return EXIT_SUCCESS;
 }
