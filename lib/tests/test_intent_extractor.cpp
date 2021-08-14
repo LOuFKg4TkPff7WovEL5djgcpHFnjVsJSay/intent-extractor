e8e581f946e71c9950de489be80d77cf59d1beb6
LOuFKg4TkPff7WovEL5djgcpHFnjVsJSay <88899835+LOuFKg4TkPff7WovEL5djgcpHFnjVsJSay@users.noreply.github.com>
2021-08-14 11:36:28 +0200

    Add multiple intent unit test
    Check that the returned intent is the first matching intent. We don't
    have any other logic to handle multiple intents.

--- lib/tests/test_intent_extractor.cpp
+++ lib/tests/test_intent_extractor.cpp
@@ -3,22 +3,33 @@
 #include <lib.hpp>
 
 struct TestIntent final : ie::Intent {
-    std::optional<std::size_t> location;
-
     inline static std::string_view NAME = "test_intent";
 
     std::string_view name() const override { return NAME; }
 
-    void scan_for_entities(std::size_t location_of_intent,
+    void scan_for_entities(std::size_t,
                            std::string_view,
-                           std::span<ie::Token> tokens) override {
-        location = tokens[location_of_intent].start;
+                           std::span<ie::Token>) override {
     }
 
     void log_result(std::ostream& stream) const override { stream << "Test Intent"; }
 };
 
+struct TestIntent2 final : ie::Intent {
+    inline static std::string_view NAME = "test_intent2";
+
+    std::string_view name() const override { return NAME; }
+
+    void scan_for_entities(std::size_t,
+                           std::string_view,
+                           std::span<ie::Token>) override {
+    }
+
+    void log_result(std::ostream& stream) const override { stream << "Test Intent2"; }
+};
+
 IE_DERIVE_INTENT_BUILD(TestIntent);
+IE_DERIVE_INTENT_BUILD(TestIntent2);
 
 struct IntentExtractorTest {
     ie::IntentExtractor extractor;
@@ -26,11 +37,21 @@ struct IntentExtractorTest {
     IntentExtractorTest() {
         const ie::InternedString key_words[] = {
             extractor.string_interner().get_or_intern(std::string_view("test")),
+            extractor.string_interner().get_or_intern(std::string_view("test_alt")),
+        };
+        const ie::InternedString key_words2[] = {
             extractor.string_interner().get_or_intern(std::string_view("test2")),
         };
-        const auto register_result = extractor.intent_factory().register_builder(
-            std::make_shared<TestIntentBuilder>(), key_words);
-        REQUIRE(register_result == ie::IntentFactoryRegistrationResult::Ok);
+        {
+            const auto register_result = extractor.intent_factory().register_builder(
+                    std::make_shared<TestIntentBuilder>(), key_words);
+            REQUIRE(register_result == ie::IntentFactoryRegistrationResult::Ok);
+        }
+        {
+            const auto register_result = extractor.intent_factory().register_builder(
+                    std::make_shared<TestIntent2Builder>(), key_words2);
+            REQUIRE(register_result == ie::IntentFactoryRegistrationResult::Ok);
+        }
     }
 };
 
@@ -43,5 +64,16 @@ TEST_CASE_METHOD(IntentExtractorTest, "IntentExtractor: basic test") {
         CHECK(result->start_of_keyword == 10);
         CHECK(result->length_of_keyword == 4);
         CHECK(input.substr(result->start_of_keyword, result->length_of_keyword) == "Test");
+    }
+}
+
+TEST_CASE_METHOD(IntentExtractorTest, "IntentExtractor: more than one intent") {
+    // Check that the returned result is the first matching intent
+    const std::string_view input = "This ,test2, is a Test!";
+    const auto result = extractor.extract_intent(input);
+    CHECK(result);
+    if (result) {
+        CHECK(result->intent->name() == TestIntent2::NAME);
+        CHECK(input.substr(result->start_of_keyword, result->length_of_keyword) == "test2");
     }
 }
